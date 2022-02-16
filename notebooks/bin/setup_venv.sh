76d6a94b59b4dd254ced6825b6bf12d7e57b9df0
d. h. bradshaw <dhbradshaw@gmail.com>
2022-02-16 08:28:32 -0600

    Create and name nnums jupyter kernal

--- notebooks/bin/setup_venv.sh
+++ notebooks/bin/setup_venv.sh
@@ -1,7 +1,16 @@
+# Create the virtual environment  
 python3.10 -m venv venv
+
+# Activate the virtual environment
 source venv/bin/activate
+
+# Install and compile the requirements
 pip install --upgrade pip
 pip install pip-tools
 cd requirements
 pip-compile --output-file requirements.txt requirements.in
 pip install -r requirements.txt
+
+# Create and name the kernal for this virtual environment
+cd ..
+python -m ipykernel install --user --name nnums --display-name "nnums.com"
